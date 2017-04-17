2674fb86ebbad6808dd82445629fbe9025384c4a
larunrahul <rahul.arun86@gmail.com>
2017-04-17 23:59:17 +0530

    adding explanation to problem 02

--- src/problems/Problem_02_Even_Valued_Fibanocci.java
+++ src/problems/Problem_02_Even_Valued_Fibanocci.java
@@ -3,6 +3,9 @@ package problems;
 public class Problem_02_Even_Valued_Fibanocci {
 
 	public static void main(String[] args) {
+		/*
+		 * Even valued Fibanocci numbers are nothing but every third number in the Fibanocci Series starting from 2
+		 */
 		long prev = 1, sum = 2, prevSum = sum;
 		while(prevSum < 4000000) {
 			long first = prev + prevSum;
